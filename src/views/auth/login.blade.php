bec724fb3717b29b499daa2da7d1bfbc645799df
Tim Van den Bergh <tim@fikst.be>
2014-08-14 14:02:40 +0200

    login template update

--- src/views/auth/login.blade.php
+++ src/views/auth/login.blade.php
@@ -1,26 +1,67 @@
-@extends('cms::template.layout')
+<!DOCTYPE html>
+<html lang="en">
 
-@section('content')
-    <h1>Login</h1>
+<head>
 
-    {{ Form::open( ['route' => 'cms.login'] ) }}
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <div class="form-group">
-            {{ Form::label('email', 'Email') }}
-            {{ Form::text('email', null, ['class' => 'form-control']) }}
-            {{ $errors->first('email', '<span class="error">:message</span>') }}
+    <title>{{ $cmsTitle }}</title>
+
+    <link href="{{ asset('packages/teman/cms/css/bootstrap.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('packages/teman/cms/css/bootstrap-theme.min.css') }}" rel="stylesheet">
+
+    <link href="{{ asset('packages/teman/cms/css/style.css') }}" rel="stylesheet">
+
+</head>
+
+<body>
+
+<div class="container-fluid">
+
+
+    <div class="row">
+
+        <div class="col-sm-4 col-sm-offset-4">
+
+            @if (Session::has('flash_message'))
+            <div class="alert alert-info" role="alert">{{ Session::get('flash_message') }}</div>
+            @endif
+
+            <h1>Login</h1>
+
+            {{ Form::open( ['route' => 'cms.login'] ) }}
+
+                <div class="form-group">
+                    {{ Form::label('email', 'Email') }}
+                    {{ Form::text('email', null, ['class' => 'form-control']) }}
+                    {{ $errors->first('email', '<span class="error">:message</span>') }}
+                </div>
+
+                <div class="form-group">
+                    {{ Form::label('password', 'Password') }}
+                    {{ Form::password('password', ['class' => 'form-control']) }}
+                    {{ $errors->first('password', '<span class="error">:message</span>') }}
+                </div>
+
+                <div class="form-group">
+                    {{ Form::submit('Log in', ['class' => 'btn btn-primary']) }}
+                </div>
+
+            {{ Form::close() }}
+
         </div>
-    
-        <div class="form-group">
-            {{ Form::label('password', 'Password') }}
-            {{ Form::password('password', ['class' => 'form-control']) }}
-            {{ $errors->first('password', '<span class="error">:message</span>') }}
-        </div>
 
-        <div class="form-group">
-            {{ Form::submit('Log in', ['class' => 'btn btn-primary']) }}
-        </div>
+    </div>
 
-    {{ Form::close() }}
 
-@stop
+
+</div>
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+<script src="{{ asset('packages/teman/cms/js/bootstrap.min.js') }}"></script>
+
+</body>
+
+</html>
