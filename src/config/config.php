f676fd9735dff99561c32616f31394cac2f8ca03
nick smet <nick.smet@bagaar.be>
2014-09-03 11:43:39 +0200

    Config file wasn't being published to the packages folder, config is now added

---
+++ src/config/config.php
@@ -0,0 +1,41 @@
+<?php return array(
+
+
+    /*
+     * The default title to display in the menu bar
+     */
+    'cmsTitle' => 'Teman CMS',
+
+    /*
+     * Menu items for admin interface
+     *
+     */
+    'categorieItems' => [
+        [
+            'title' => 'Users',
+            'permission'=>'acces_cms',
+            'subCategorieItems' => [
+                 [
+                    'permission'=>'acces_cms',
+                    'route' => 'admin.users.index',
+                    'itemText'=>'Users',
+                    'Iclass'=>'fa fa-user',
+                 ],
+            ],
+        ],
+
+        [
+            'title' => 'Textbox',
+            'permission'=>'acces_cms',
+            'subCategorieItems' => [
+                [
+                    'permission'=>'acces_cms',
+                    'route' => 'admin.textbox.index',
+                    'itemText'=>'Rich Textbox Editor',
+                    'Iclass'=>'fa fa-file-text'
+                ],
+            ]
+        ]
+    ],
+
+);
